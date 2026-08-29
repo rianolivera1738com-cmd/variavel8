255a86a2fcd9e0e63f9ed500647d10f8eef1d39b
Rian Oliveira Dos Santos <rianolivera1738.com@gmail.com>
2026-08-29 05:28:26 -0300

    variavel8

--- Add.java
+++ Add.java
@@ -4,8 +4,10 @@ String nomeFuncionario = "Carlos Silva";
 double valorHora = 35.50;
 double horasTrabalhadas = 160.0; 
 double salarioMensal = valorHora * horasTrabalhadas;
+double salarioBruto = valorHora * horasTrabalhadas;
 System.out.println("Funcionário: " + nomeFuncionario);
 System.out.println("Valor da hora: R$ " + valorHora);
 System.out.println("Horas trabalhadas: " + horasTrabalhadas);
 System.out.println("Salário mensal: R$ " + salarioMensal);
+System.out.println("Salário bruto: R$ " + salarioBruto);
 }}
