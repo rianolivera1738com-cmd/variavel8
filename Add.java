cabad3f7f69ece93a43fe96e4278a94362a8cecb
Rian Oliveira Dos Santos <rianolivera1738.com@gmail.com>
2026-08-29 08:24:07 +0000

    variavel8

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class App {
+public static void main(String[] args) throws Exception {
+String nomeFuncionario = "Carlos Silva";
+double valorHora = 35.50;
+double horasTrabalhadas = 160.0; 
+double salarioMensal = valorHora * horasTrabalhadas;
+System.out.println("Funcionário: " + nomeFuncionario);
+System.out.println("Valor da hora: R$ " + valorHora);
+System.out.println("Horas trabalhadas: " + horasTrabalhadas);
+System.out.println("Salário mensal: R$ " + salarioMensal);
+}}
